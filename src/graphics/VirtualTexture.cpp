e4dfdb9270b7d4ed23bef54dd17038ddd443a777
Jonah Shader <jonah.shader@gmail.com>
2021-09-23 01:23:51 -0400

    idk what im doing

---
+++ src/graphics/VirtualTexture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jonah on 9/23/2021.
+//
+
+#include "VirtualTexture.h"
---
+++ src/graphics/VirtualTexture.h
@@ -0,0 +1,20 @@
+//
+// Created by Jonah on 9/23/2021.
+//
+
+#ifndef LEARNOPENGL_VOXEL_EXPERIMENT_VIRTUALTEXTURE_H
+#define LEARNOPENGL_VOXEL_EXPERIMENT_VIRTUALTEXTURE_H
+
+#include <vector>
+#include <string>
+
+class VirtualTexture {
+public:
+    VirtualTexture();
+
+private:
+    std::vector<std::string> files;
+};
+
+
+#endif //LEARNOPENGL_VOXEL_EXPERIMENT_VIRTUALTEXTURE_H
